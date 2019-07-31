837be6fd5a671cf55e4de0ba2b7af1cc1092e506
shinichi55 <tanno.showa56@gmail.com>
2019-07-30 21:18:46 -1000

    :tada: First commit

---
+++ system_info_tool.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# System Info Tool - This tool will grab the hostname of the target machine.
+# This tool will need to use the socket module.
+# It will print th ehostname to stdout in the format of: "The hostname is: NoSoupForYou"
+
+import socket
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="System Info Tool",
+    description="Will print hostname"
+)
+
+parser.add_argument("ip", help="Hostname IP address.")
+
+args = parser.parse_args()
+print( args )
+
+ip = args.ip
+hostname = socket.gethostbyaddr( ip )
+print( "The hostname is: {}".format( hostname[0] ) )
