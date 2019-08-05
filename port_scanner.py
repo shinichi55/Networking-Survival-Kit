4612785db25c22df97212ea1dabcfa97eeb05bb2
shinichi55 <you@example.com>
2019-08-05 12:04:13 -1000

    Created a function

--- port_scanner.py
+++ port_scanner.py
@@ -11,43 +11,47 @@ import argparse
 
 file = open( "port_scanner.txt", "w" )
 
-parser = argparse.ArgumentParser (
-    prog="Port Scanner",
-    description="This tool will scan for open ports on any domain or IP and return a report when completed."
-)
+def port_scanner():
+    parser = argparse.ArgumentParser (
+        prog="Port Scanner",
+        description="This tool will scan for open ports on any domain or IP and return a report when completed."
+    )
 
-parser.add_argument("ip", help="IP to scan")
-parser.add_argument("port", help="Port(s) to scan over")
+    parser.add_argument("ip", help="IP to scan")
+    parser.add_argument("port", help="Port(s) to scan over")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-try:
-    socket.gethostbyname( args.ip )
+    try:
+        socket.gethostbyname( args.ip )
 
-except OSError:
-    print( "Check input information..." )
+    except OSError:
+        print( "Check input information..." )
 
-else:
-    if '-' in args.port:
-        ip_range = args.port.split( '-' )
-        x = int( ip_range[0] )
-        y = int( ip_range[1] )
-        for ip in range( x, y ):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
-            result = sock.connect_ex( ( socket.gethostbyname( args.ip ), int( ip ) ) )
-            if result == 0:
-                file.write( "Port {}: Open".format( ip ) + "\n" )
-            else:
-                file.write( "Port {}: Closed".format( ip ) + "\n" )
-            sock.close()
     else:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex( ( socket.gethostbyname( args.ip ), int( args.port ) ) )
-        if result == 0:
-            file.write( "Port {}: Open".format( args.port ) )
+        if '-' in args.port:
+            ip_range = args.port.split( '-' )
+            x = int( ip_range[0] )
+            y = int( ip_range[1] )
+            for ip in range( x, y ):
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(1)
+                result = sock.connect_ex( ( socket.gethostbyname( args.ip ), int( ip ) ) )
+                if result == 0:
+                    file.write( "Port {}: Open".format( ip ) + "\n" )
+                else:
+                    file.write( "Port {}: Closed".format( ip ) + "\n" )
+                sock.close()
         else:
-            file.write( "Port {}: Closed".format( args.port ) )
-        sock.close()
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex( ( socket.gethostbyname( args.ip ), int( args.port ) ) )
+            if result == 0:
+                file.write( "Port {}: Open".format( args.port ) )
+            else:
+                file.write( "Port {}: Closed".format( args.port ) )
+            sock.close()
+
+if __name__ == "__main__":
+    port_scanner()
 
 file.close()
