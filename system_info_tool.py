ac9ddba165b05deb4cbb213e87a4da399a41c6b3
shinichi55 <you@example.com>
2019-08-05 12:03:49 -1000

    Created a function

--- system_info_tool.py
+++ system_info_tool.py
@@ -7,20 +7,24 @@
 import socket
 import argparse
 
-parser = argparse.ArgumentParser(
-    prog="System Info Tool",
-    description="This tool will grab the hostname of the target machine."
-)
+def system_info_tool():
+    parser = argparse.ArgumentParser(
+        prog="System Info Tool",
+        description="This tool will grab the hostname of the target machine."
+    )
 
-parser.add_argument("ip", help="IP address of Hostname.")
+    parser.add_argument("ip", help="IP address of Hostname.")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-try:
-    socket.gethostbyaddr( args.ip )
+    try:
+        socket.gethostbyaddr( args.ip )
 
-except OSError:
-    print( "Input valid IP address" )
+    except OSError:
+        print( "Input valid IP address" )
 
-else:
-    print( "The hostname is: {}".format( socket.gethostbyaddr( args.ip )[0] ) )
+    else:
+        print( "The hostname is: {}".format( socket.gethostbyaddr( args.ip )[0] ) )
+
+if __name__ == "__main__":
+    system_info_tool()
