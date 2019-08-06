597c4419dc92a66e9724314e769712e4194e502e
shinichi55 <you@example.com>
2019-08-05 20:56:54 -1000

    Changed output formatting

--- port_scanner.py
+++ port_scanner.py
@@ -26,7 +26,7 @@ def port_scanner():
         socket.gethostbyname( args.ip )
 
     except OSError:
-        print( "Check input information..." )
+        print( "Check input information...\n" )
 
     else:
         if '-' in args.port:
@@ -38,17 +38,17 @@ def port_scanner():
                 sock.settimeout(1)
                 result = sock.connect_ex( ( socket.gethostbyname( args.ip ), int( ip ) ) )
                 if result == 0:
-                    file.write( "Port {}: Open".format( ip ) + "\n" )
+                    file.write( "Port {}: Open\n".format( ip ) )
                 else:
-                    file.write( "Port {}: Closed".format( ip ) + "\n" )
+                    file.write( "Port {}: Closed\n".format( ip ) )
                 sock.close()
         else:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             result = sock.connect_ex( ( socket.gethostbyname( args.ip ), int( args.port ) ) )
             if result == 0:
-                file.write( "Port {}: Open".format( args.port ) )
+                file.write( "Port {}: Open\n".format( args.port ) )
             else:
-                file.write( "Port {}: Closed".format( args.port ) )
+                file.write( "Port {}: Closed\n".format( args.port ) )
             sock.close()
 
 if __name__ == "__main__":
